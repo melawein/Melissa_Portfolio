4cb1628d34e38773d728c30a4f814ebfe23c899e
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-08-28 13:49:28 +0300

    create projects page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'resume', to: 'pages#resume', as: :resume
+  get 'aboutme', to: 'pages#aboutme', as: :aboutme
+  get 'projects', to: 'pages#projects', as: :projects
+
 end
